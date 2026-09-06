c8c0392b011b80068c95bec674f9164616b5b260
Prashantdev780 <prashant.sharma@opstree.com>
2026-09-06 15:55:42 +0530

    Create build.groovy

---
+++ jenkins-job-dsl/jobs/build.groovy
@@ -0,0 +1,19 @@
+job('demo-folder/build-job'){
+  description('job created by dsl')
+
+scm {
+    git {
+      remote{
+        url('https://github.com/adrianhajdin/chat_application.git')
+      }
+      branch('main')
+    }
+}
+steps {
+  shell('''
+    echo "build job"
+    echo "repo clone"
+  ''')
+}
+  
+}
