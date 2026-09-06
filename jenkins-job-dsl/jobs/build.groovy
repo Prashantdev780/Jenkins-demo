026700edecd9f14313ebc60b063508cac7197585
Prashantdev780 <prashant.sharma@opstree.com>
2026-09-06 16:17:51 +0530

    Update build.groovy

--- jenkins-job-dsl/jobs/build.groovy
+++ jenkins-job-dsl/jobs/build.groovy
@@ -17,3 +17,65 @@ steps {
 }
   
 }
+
+job('build-job2'){
+  description('job created by dsl')
+
+scm {
+    git {
+      remote{
+        url('https://github.com/adrianhajdin/chat_application.git')
+      }
+      branch('main')
+    }
+}
+steps {
+  shell('''
+    echo "build job"
+    echo "repo clone"
+  ''')
+}
+  
+}
+
+
+job('build-job3'){
+  description('job created by dsl')
+
+scm {
+    git {
+      remote{
+        url('https://github.com/adrianhajdin/chat_application.git')
+      }
+      branch('main')
+    }
+}
+steps {
+  shell('''
+    echo "build job"
+    echo "repo clone"
+  ''')
+}
+  
+}
+
+
+job('build-job4'){
+  description('job created by dsl')
+
+scm {
+    git {
+      remote{
+        url('https://github.com/adrianhajdin/chat_application.git')
+      }
+      branch('main')
+    }
+}
+steps {
+  shell('''
+    echo "build job"
+    echo "repo clone"
+  ''')
+}
+  
+}
